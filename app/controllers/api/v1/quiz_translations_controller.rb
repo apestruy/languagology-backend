afa031b486286fb71c934c35cdb6984ae0d1aaa7
Sasha <apestruy@gmail.com>
2020-09-24 02:36:42 -0400

    updated controllers to create quiz coming from frontend

--- app/controllers/api/v1/quiz_translations_controller.rb
+++ app/controllers/api/v1/quiz_translations_controller.rb
@@ -3,4 +3,15 @@ class Api::V1::QuizTranslationsController < ApplicationController
         @quiz_translations = QuizTranslation.all
         render json: @quiz_translations
     end
+
+    def create
+        @quiz_translation = QuizTranslation.create!(quiz_translation_params)
+        render json: @quiz_translation
+    end
+
+    private
+
+    def quiz_translation_params
+        params.require(:quiz_translation).permit(:quiz_id, :translation_id, :correct)
+    end
 end
--- app/controllers/api/v1/quizzes_controller.rb
+++ app/controllers/api/v1/quizzes_controller.rb
@@ -3,4 +3,15 @@ class Api::V1::QuizzesController < ApplicationController
         @quizzes = Quiz.all
         render json: @quizzes
     end
+
+    def create
+        @quiz = Quiz.create!(quiz_params)
+        render json: @quiz
+    end
+
+    private
+
+    def quiz_params
+        params.require(:quiz).permit(:score, :user_id)
+    end 
 end
